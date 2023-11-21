74360a151b54c9feff53f303cec9f7cab73b458c
evfjunior <44171363+evfjunior@users.noreply.github.com>
2023-11-21 09:58:43 -0300

    feat: add eslint configuration for node

---
+++ node.js
@@ -0,0 +1,34 @@
+module.exports = {
+  root: true,
+  env: {
+    es2021: true,
+    node: true,
+    jest: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'standard-with-typescript'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: [
+        '.eslintrc.{js,cjs}'
+      ],
+      parserOptions: {
+        sourceType: 'script'
+      }
+    }
+  ],
+  plugins: ['@typescript-eslint'],
+  rules: {
+  }
+}
